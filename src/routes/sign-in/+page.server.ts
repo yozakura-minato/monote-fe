f38af52249a10896b5a7e0a3b27dca7ec4813e99
yozakura-minato <huynhhuulocht@gmail.com>
2026-07-19 10:06:19 +0700

    feature/authentication: refactor for api fetching

--- src/routes/sign-in/+page.server.ts
+++ src/routes/sign-in/+page.server.ts
@@ -1,4 +1,4 @@
-import { fail, superValidate, message, type SuperValidated } from 'sveltekit-superforms';
+import { fail, superValidate, message } from 'sveltekit-superforms';
 import { valibot } from 'sveltekit-superforms/adapters';
 import { SignInSchema } from './Schema.js';
 import type { Actions, PageServerLoad } from './$types.js';
@@ -12,9 +12,7 @@ export const actions: Actions = {
     default: async ({ request, fetch }) => {
         const form = await superValidate(request, valibot(SignInSchema));
 
-        if (!form.valid) {
-            return fail(400, { form });
-        }
+        if (!form.valid) return fail(400, { form });
 
         try {
             const response = await fetch('http://localhost:8080/api/sign-in', {
@@ -22,19 +20,19 @@ export const actions: Actions = {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(form.data)
             });
-
+            const responseBody = await response.json().catch(() => null);
+            
             if (!response.ok) {
-                const errorData = await response
-                    .json()
-                    .catch(() => ({ error: 'general.badRequest' }));
-
                 return message(
                     form,
-                    { type: 'error', text: errorData.error ?? 'general.badRequest' },
+                    { type: 'error', text: responseBody?.error ?? 'general.badRequest' },
                     { status: response.status as any }
                 );
             }
 
+            console.log(responseBody?.data.accessToken);
+            console.log(responseBody?.data.refreshToken);
+            
             return message(
                 form,
                 { type: 'success', text: 'signIn.success' }
